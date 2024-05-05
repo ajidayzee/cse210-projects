2bcc28ae677f98bec858ae711b4838922b245b46
ajidayzee <ajidayzee123@gmail.com>
2024-05-05 23:02:54 +0100

    New Commit

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("This is in C#");
+
+        int number = 5;
+        number = 8;
+        number = number +3;
+        Console.WriteLine($"Number is {number}");
+
+        string color = "blue";
+        Console.WriteLine($"The colour of my clothe is {color}");
+        Console.WriteLine("The colour of my clothe is" + " " + color);
+
+        Console.WriteLine("What is your Name?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"My name is {name}");
+        Console.WriteLine("My name is" + " " + name );
+
+        if (number > 3)
+        {
+            Console.WriteLine($"Number is greater");
+            Console.WriteLine("");
+        }
     }
 }
